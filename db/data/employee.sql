9f7fad04efb67d84bd63e738127a32565d3d637b
tirzasrwn <tirzasrwn@gmail.com>
2025-05-31 14:50:53 +0700

    feat: Store user accounts in database.

--- db/data/employee.sql
+++ db/data/employee.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `employee`;
-USE `employee`;
+CREATE DATABASE IF NOT EXISTS `employee_directory`;
+
+USE `employee_directory`;
 
 --
 -- Table structure for table `employee`
 --
-
 DROP TABLE IF EXISTS `employee`;
 
 CREATE TABLE `employee` (
@@ -13,16 +13,63 @@ CREATE TABLE `employee` (
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = latin1;
 
 --
 -- Data for table `employee`
 --
+INSERT INTO
+  `employee`
+VALUES
+  (1, 'Leslie', 'Andrews', 'leslie@luv2code.com'),
+  (2, 'Emma', 'Baumgarten', 'emma@luv2code.com'),
+  (3, 'Avani', 'Gupta', 'avani@luv2code.com'),
+  (4, 'Yuri', 'Petrov', 'yuri@luv2code.com'),
+  (5, 'Juan', 'Vega', 'juan@luv2code.com');
 
-INSERT INTO `employee` VALUES 
-	(1,'Leslie','Andrews','leslie@mail.com'),
-	(2,'Emma','Baumgarten','emma@mail.com'),
-	(3,'Avani','Gupta','avani@mail.com'),
-	(4,'Yuri','Petrov','yuri@mail.com'),
-	(5,'Juan','Vega','juan@mail.com');
+DROP TABLE IF EXISTS `authorities`;
 
+DROP TABLE IF EXISTS `users`;
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+-- Inserting data for table `users`
+--
+INSERT INTO
+  `users`
+VALUES
+  ('john', '{noop}test123', 1),
+  ('mary', '{noop}test123', 1),
+  ('susan', '{noop}test123', 1);
+
+--
+-- Table structure for table `authorities`
+--
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+-- Inserting data for table `authorities`
+--
+INSERT INTO
+  `authorities`
+VALUES
+  ('john', 'ROLE_EMPLOYEE'),
+  ('mary', 'ROLE_EMPLOYEE'),
+  ('mary', 'ROLE_MANAGER'),
+  ('susan', 'ROLE_EMPLOYEE'),
+  ('susan', 'ROLE_MANAGER'),
+  ('susan', 'ROLE_ADMIN');
--- src/main/java/com/tirzasrwn/app/employee/security/DemoSecurityConfig.java
+++ src/main/java/com/tirzasrwn/app/employee/security/DemoSecurityConfig.java
@@ -1,38 +1,23 @@
 package com.tirzasrwn.app.employee.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
 	@Bean
-	public InMemoryUserDetailsManager userDetailsManager() {
+	public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-		UserDetails john = User.builder()
-				.username("john")
-				.password("{noop}test123")
-				.roles("EMPLOYEE")
-				.build();
-		UserDetails mary = User.builder()
-				.username("mary")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER")
-				.build();
-		UserDetails susan = User.builder()
-				.username("susan")
-				.password("{noop}test123")
-				.roles("EMPLOYEE", "MANAGER", "ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(john, mary, susan);
-
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	@Bean
