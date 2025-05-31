e185fdc3d5636031560113c8ede71933d62ddf5a
tirzasrwn <tirzasrwn@gmail.com>
2025-05-31 14:21:55 +0700

    feat: Add patch endpoint for partial update.

--- src/main/java/com/tirzasrwn/app/employee/rest/EmployeeRestController.java
+++ src/main/java/com/tirzasrwn/app/employee/rest/EmployeeRestController.java
@@ -3,16 +3,21 @@ package com.tirzasrwn.app.employee.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.tirzasrwn.app.employee.entity.Employee;
 import com.tirzasrwn.app.employee.service.EmployeeService;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
 
 	private EmployeeService employeeService;
+
+	private ObjectMapper objectMapper;
 
 	@Autowired
 	public EmployeeRestController(EmployeeService theEmployeeService) {
@@ -62,6 +67,46 @@ public class EmployeeRestController {
 		Employee dbEmployee = employeeService.save(theEmployee);
 
 		return dbEmployee;
+	}
+
+	// add mapping for PATCH /employees/{employeeId} - patch employee ... partial
+	// update
+
+	@PatchMapping("/employees/{employeeId}")
+	public Employee patchEmployee(@PathVariable int employeeId,
+			@RequestBody Map<String, Object> patchPayload) {
+
+		Employee tempEmployee = employeeService.findById(employeeId);
+
+		// throw exception if null
+		if (tempEmployee == null) {
+			throw new RuntimeException("Employee id not found - " + employeeId);
+		}
+
+		// throw exception if request body contains "id" key
+		if (patchPayload.containsKey("id")) {
+			throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+		}
+
+		Employee patchedEmployee = apply(patchPayload, tempEmployee);
+
+		Employee dbEmployee = employeeService.save(patchedEmployee);
+
+		return dbEmployee;
+	}
+
+	private Employee apply(Map<String, Object> patchPayload, Employee tempEmployee) {
+
+		// Convert employee object to a JSON object node
+		ObjectNode employeeNode = objectMapper.convertValue(tempEmployee, ObjectNode.class);
+
+		// Convert the patchPayload map to a JSON object node
+		ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+
+		// Merge the patch updates into the employee node
+		employeeNode.setAll(patchNode);
+
+		return objectMapper.convertValue(employeeNode, Employee.class);
 	}
 
 	// add mapping for DELETE /employees/{employeeId} - delete employee
