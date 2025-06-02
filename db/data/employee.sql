6351a92d33836e2d68f3624c4476c8a648703b20
tirzasrwn <tirzasrwn@gmail.com>
2025-06-02 08:25:54 +0700

    feat: Implement bcript.

--- db/data/employee.sql
+++ db/data/employee.sql
@@ -36,7 +36,7 @@ DROP TABLE IF EXISTS `users`;
 --
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` char(68) NOT NULL,
   `enabled` tinyint NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
@@ -44,12 +44,30 @@ CREATE TABLE `users` (
 --
 -- Inserting data for table `users`
 --
+-- NOTE: The passwords are encrypted using BCrypt
+--
+-- A generation tool is avail at: https://www.luv2code.com/generate-bcrypt-password
+--
+-- Default passwords here are: fun123
+--
 INSERT INTO
   `users`
 VALUES
-  ('john', '{noop}test123', 1),
-  ('mary', '{noop}test123', 1),
-  ('susan', '{noop}test123', 1);
+  (
+    'john',
+    '{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',
+    1
+  ),
+  (
+    'mary',
+    '{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',
+    1
+  ),
+  (
+    'susan',
+    '{bcrypt}$2a$10$qeS0HEh7urweMojsnwNAR.vcXJeXR1UcMRZ2WcGQl9YeuspUdgF.q',
+    1
+  );
 
 --
 -- Table structure for table `authorities`
@@ -57,8 +75,8 @@ VALUES
 CREATE TABLE `authorities` (
   `username` varchar(50) NOT NULL,
   `authority` varchar(50) NOT NULL,
-  UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
-  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+  UNIQUE KEY `authorities4_idx_1` (`username`, `authority`),
+  CONSTRAINT `authorities4_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 --
