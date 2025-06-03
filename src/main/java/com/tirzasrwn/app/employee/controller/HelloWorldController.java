731855443ddfd8fe569bbcc4e38b3ec7e355729d
tirzasrwn <tirzasrwn@gmail.com>
2025-06-03 09:39:18 +0700

    feat: Adding data to spring model.

--- src/main/java/com/tirzasrwn/app/employee/controller/HelloWorldController.java
+++ src/main/java/com/tirzasrwn/app/employee/controller/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.tirzasrwn.app.employee.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -14,5 +17,19 @@ public class HelloWorldController {
 	@RequestMapping("/processForm")
 	public String processForm() {
 		return "helloworld";
+	}
+
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		// read the request parameter from the HTML form
+		String theName = request.getParameter("studentName");
+		// convert the data to all caps
+		theName = theName.toUpperCase();
+		// create the message
+		String result = "Yo! " + theName;
+		// add message to the model
+		model.addAttribute("message", result);
+		return "helloworld";
+
 	}
 }
