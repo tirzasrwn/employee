e42df7186bc522d95e70ff4e5923fa88c7d9fcec
tirzasrwn <tirzasrwn@gmail.com>
2025-06-06 18:53:36 +0700

    feat: Implement list employees.

---
+++ src/main/java/com/tirzasrwn/app/employee/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.tirzasrwn.app.employee.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tirzasrwn.app.employee.entity.Employee;
+import com.tirzasrwn.app.employee.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+	private EmployeeService employeeService;
+
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		List<Employee> theEmployees = employeeService.findAll();
+		System.out.println("->" + theEmployees);
+		theModel.addAttribute("employees", theEmployees);
+
+		return "list-employees";
+	}
+
+}
