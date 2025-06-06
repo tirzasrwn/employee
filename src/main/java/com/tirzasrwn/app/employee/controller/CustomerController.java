7b77bb55e0f91e0740058e5fda3147201ff381af
tirzasrwn <tirzasrwn@gmail.com>
2025-06-06 08:45:26 +0700

    feat: Using @InitBinder.

--- src/main/java/com/tirzasrwn/app/employee/controller/CustomerController.java
+++ src/main/java/com/tirzasrwn/app/employee/controller/CustomerController.java
@@ -1,9 +1,12 @@
 package com.tirzasrwn.app.employee.controller;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -31,6 +34,12 @@ public class CustomerController {
 		} else {
 			return "customer-confirmation";
 		}
+	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
 	}
 
 }
