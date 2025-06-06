03c46cf4f1b4b12f389b2f692357221e647f96fd
tirzasrwn <tirzasrwn@gmail.com>
2025-06-06 09:49:46 +0700

    feat: Spring MVC form validation. Number range.

--- src/main/java/com/tirzasrwn/app/employee/model/Customer.java
+++ src/main/java/com/tirzasrwn/app/employee/model/Customer.java
@@ -1,5 +1,7 @@
 package com.tirzasrwn.app.employee.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -9,6 +11,10 @@ public class Customer {
 	@NotNull(message = "is required")
 	@Size(min = 1, message = "is required")
 	private String lastName = "";
+
+	@Min(value = 0, message = "must be greater than or equal to zero")
+	@Max(value = 10, message = "must be less than or equal to 10")
+	private int freePasses;
 
 	public String getFirstName() {
 		return firstName;
@@ -29,6 +35,14 @@ public class Customer {
 	@Override
 	public String toString() {
 		return "Customer []";
+	}
+
+	public int getFreePasses() {
+		return freePasses;
+	}
+
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
 	}
 
 }
