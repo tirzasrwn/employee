f368513949e2c9506011212724cc0ca2b2b326c1
tirzasrwn <tirzasrwn@gmail.com>
2025-06-13 15:22:36 +0700

    feat: Implement add an employee.

--- src/main/java/com/tirzasrwn/app/employee/controller/EmployeeController.java
+++ src/main/java/com/tirzasrwn/app/employee/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tirzasrwn.app.employee.entity.Employee;
@@ -22,10 +24,26 @@ public class EmployeeController {
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 		List<Employee> theEmployees = employeeService.findAll();
-		System.out.println("->" + theEmployees);
 		theModel.addAttribute("employees", theEmployees);
 
-		return "list-employees";
+		return "employees/list-employees";
 	}
 
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+
+		theModel.addAttribute("employee", theEmployee);
+
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String save(@ModelAttribute("employee") Employee theEmployee) {
+		employeeService.save(theEmployee);
+
+		return "redirect:/employees/list";
+	}
 }
